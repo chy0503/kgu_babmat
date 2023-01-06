ddbc88b1a8eea9602cd584027afef780ae5d6cd0
choheeyeon <202015006@kyonggi.ac.kr>
2023-01-06 15:31:57 +0900

    :tada: begin a project

---
+++ src/main/java/kyonggi_girls/kgu_babmat/KguBabmatApplication.java
@@ -0,0 +1,13 @@
+package kyonggi_girls.kgu_babmat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KguBabmatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KguBabmatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kyonggi_girls/kgu_babmat/KguBabmatApplicationTests.java
@@ -0,0 +1,13 @@
+package kyonggi_girls.kgu_babmat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KguBabmatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
