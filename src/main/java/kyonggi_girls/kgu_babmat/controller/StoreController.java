c3a747423affa3abd177d9bb4d3a546cedadc6ff
choheeyeon <202015006@kyonggi.ac.kr>
2023-01-17 01:24:24 +0900

    :sparkles: add markers on a map

--- src/main/java/kyonggi_girls/kgu_babmat/controller/StoreController.java
+++ src/main/java/kyonggi_girls/kgu_babmat/controller/StoreController.java
@@ -1,12 +1,15 @@
 package kyonggi_girls.kgu_babmat.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StoreController {
     @GetMapping("store")
-    public String store() {
+    public String store(@RequestParam("storeName") String storeName, Model model) {
+        model.addAttribute("storeName", storeName);
         return "store";
     }
 }
