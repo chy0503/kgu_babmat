1f59082f4e7745b055583dd6f456c5168d18148d
leejeongeun98 <je981@naver.com>
2023-01-17 21:51:08 +0900

    :memo: connect review's database

--- src/main/java/kyonggi_girls/kgu_babmat/controller/reviews/ReviewController.java
+++ src/main/java/kyonggi_girls/kgu_babmat/controller/reviews/ReviewController.java
@@ -1,12 +1,32 @@
 package kyonggi_girls.kgu_babmat.controller.reviews;
 
+import kyonggi_girls.kgu_babmat.domain.storeReview;
+import kyonggi_girls.kgu_babmat.service.ReviewService;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 
 @Controller
 public class ReviewController {
-    @GetMapping("review")
-    public String review() {
-        return "reviews/review";
+
+    ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
     }
+
+    @GetMapping("/reviews/review")
+    public String reviewPage(Model model) throws Exception {
+        List<storeReview> reviewList = reviewService.getReviews();
+        model.addAttribute("reviewList", reviewList);
+        return "/reviews/review";
+    }
+
+
 }
+
--- src/main/java/kyonggi_girls/kgu_babmat/service/UserService.java
+++ src/main/java/kyonggi_girls/kgu_babmat/service/UserService.java
@@ -7,4 +7,5 @@ import java.util.concurrent.ExecutionException;
 
 public interface UserService {
     List<User> getUsers() throws ExecutionException, InterruptedException;
+
 }
