d9b12bdeef38b52da804ebba73e6202740c29a60
choheeyeon <202015006@kyonggi.ac.kr>
2023-02-24 22:26:13 +0900

    :sparkles: add logo font, search with Enter function, search controller mapping.

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
     $('.li').click(function () {
         $(this).toggleClass('jbBox');
     });
-});
-
-$(document).ready(function(){
-    $(".arrow-drop").click(function() {
+    $("#search").keydown(function (key) {
+        if ($("#search").keyCode == 13) {
+            $("#search_form").submit();
+        }
+    });
+    $(".arrow-drop").click(function () {
         var submenu = $("#weekMenu");
         if (submenu.is(":visible")) {
             submenu.slideUp();
@@ -15,10 +17,34 @@ $(document).ready(function(){
             $(".arrow-drop").css("transform", "scaleY(-1)");
         }
     });
+    $(".arrow-drop").click(function () {
+        var submenu = $("#weekMenu");
+        if (submenu.is(":visible")) {
+            submenu.slideUp();
+            $(".arrow-drop").css("transform", "scaleY(1)");
+        } else {
+            submenu.slideDown();
+            $(".arrow-drop").css("transform", "scaleY(-1)");
+        }
+    });
+
+});
+
+$(document).ready(function () {
+    $("#dial_close").click(function () {
+        $("#dial_writeReview").css("display", "none");
+    });
+    $("#write_review_btn1").click(function () {
+        $("#dial_writeReview").css("display", "block");
+    });
+    $("#write_review_btn2").click(function () {
+        $("#dial_writeReview").css("display", "block");
+    });
 });
 
 function clickLike(menu, action) {
     let f = document.createElement('form');
+
     function returnObj(name, value) {
         let obj;
         obj = document.createElement('input');
@@ -29,7 +55,7 @@ function clickLike(menu, action) {
     }
 
     f.appendChild(returnObj('menu', menu));
-    f.appendChild(returnObj('price', Number(document.getElementById(menu+'price').innerText.replace("₩", ""))));
+    f.appendChild(returnObj('price', Number(document.getElementById(menu + 'price').innerText.replace("₩", ""))));
     f.appendChild(returnObj('store', document.getElementById('storeName').innerText));
     f.appendChild(returnObj('selectStore', document.getElementById('selectStoreName').innerText));
     f.setAttribute('method', 'post');
